da452157471946056d4b8be1522cec471e471aed
Nelvi975 <nelvi_jimenez@hotmail.com>
2021-11-10 09:20:55 -0600

    actuaL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,16 +66,7 @@
                                 </div>
 
                             </li></li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-crear" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Derivacion
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown-crear">
-                                    <a class="dropdown-item" href="{{ route('Derivacion.create') }}">Crear Derivacion</a>
-                                    <a class="dropdown-item" href="{{ route('Derivacion.index') }}">Mostrar Derivacion</a>
-                                </div>
 
-                            </li></li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-crear" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Facturacion
@@ -86,6 +77,23 @@
                                 </div>
 
                             </li></li>
+
+
+                    </ul>
+                            @elseif (Auth::user()->rol=="trabajador")
+                            <li class="nav-item">
+                                <a href="{{route('home')}}" class="nav-link">dashboard</a>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-crear" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Derivacion
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown-crear">
+                                    <a class="dropdown-item" href="{{ route('Derivacion.create') }}">Crear Derivacion</a>
+                                    <a class="dropdown-item" href="{{ route('Derivacion.index') }}">Mostrar Derivacion</a>
+                                </div>
+
+                            </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-crear" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Promocion
@@ -98,20 +106,10 @@
 
 
                             </li>
+                            </ul>
+                            <ul class="navbar-nav mr-auto">
+                            </ul>
 
-                    </ul>
-                            @elseif (Auth::user()->rol=="trabajador")
-                            <li class="nav-item">
-                                <a href="{{route('home')}}" class="nav-link">dashboard</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-crear" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Queja
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown-crear">
-                                    <a class="dropdown-item" href="{{ route('Queja.create') }}">Crear Queja</a>
-                                    <a class="dropdown-item" href="{{ route('Queja.index') }}">Mostrar Queja</a>
-                                </div>
                         @endif
                         @endif
 
